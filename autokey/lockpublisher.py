356fa0f1c35b2d6295c26a769649429898d2231d
Yoshiki Obinata <mqcmd196@hotmail.co.jp>
2020-01-09 03:59:46 +0900

    fix lockpublisher

--- autokey/lockpublisher.py
+++ autokey/lockpublisher.py
@@ -4,11 +4,19 @@ from std_msgs.msg import Bool
 
 from autokey import Autokey
 
+publish_rate = rospy.Rate(2)
 face_detect = None
 
 def face_detect_cb(self, msg):
     global face_detect
     face_detect = msg
+
+class ROSKey:
+    def __init__(self):
+        rospy.init_node('door')
+        self.pub = rospy.Publisher('/door/key', String, queue_size = 1)
+        self.door_key = Autokey()
+        self.rate = publish_rate
 
 
 if __name__ == '__main__':
